7b6fdef3c3319e3a541a9f01ac4d0995836d3361
Khushal Bhalekar <khushal.bhalekar@pinnacle.in>
2025-01-29 12:26:58 +0530

    added update and delete employee logic,  corrected mindmap

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(express.json()); // its required for reading the request as json
 
 //data store as jsonarray[] of jsonobjects{}
 let employeeObj = [{
-    id:1,
+    id: 1,
     name: 'Diksha Borkar',
     location: 'Chandrapur'
 },
 {
-    id:2,
+    id: 2,
     name: 'Pravin Chaudhari',
     location: 'Gondia'
 }];
@@ -23,7 +23,7 @@ app.get('/showEmployee', (req, res) => {
     res.send(employeeObj);
 });
 
-app.post('/createEmployee',(req, res)=>{
+app.post('/createEmployee', (req, res) => {
     let empId = req.body.id;
     let empName = req.body.name;
     let empLocation = req.body.location;
@@ -36,8 +36,37 @@ app.post('/createEmployee',(req, res)=>{
     res.send(employeeObj);
 });
 
+app.put('/updateEmployee', (req, res) => {
+    let empId = req.body.id;
+    let empName = req.body.name;
+    let empLocation = req.body.location;
+
+    let tempObj = employeeObj.find(employee => employee.id == empId);
+    if (tempObj) {
+        tempObj.name = empName;
+        tempObj.location = empLocation;
+        res.send("Employee updated successfully");
+
+    } else {
+        res.send("No employee record found");
+    }
+});
+
+app.delete('/deleteEmployee', (req, res) => {
+    let empId = req.body.id;
+
+    let employeeIndex = employeeObj.findIndex(employee => employee.id == empId);
+    console.log({ employeeIndex });
+    if (employeeIndex >= 0) {
+        employeeObj.splice(employeeIndex, 1);
+        res.send("Employee deleted successfully");
+    } else {
+        res.send("No employee record found");
+    }
+});
+
 //server and port required to serve the request and give response
-app.listen(9876,()=>{
+app.listen(9876, () => {
     console.log('Server is listening on port 9876');
 });
 
